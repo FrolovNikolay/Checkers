b0164c3bb2f55970dd766cfd145bb773257b0d12
FrolovNikolay <frolov.nikolay@phystech.edu>
2015-03-23 14:28:31 +0400

    Добавлено базовое окно приложения.

---
+++ Checkers/Checkers/MainWindow.cpp
@@ -0,0 +1,59 @@
+﻿// Автор: Фролов Николай.
+
+#include <MainWindow.h>
+
+bool CMainWindow::RegisterClass()
+{
+    WNDCLASSEX windowWND;
+
+    windowWND.cbSize = sizeof( WNDCLASSEX );
+    windowWND.style = CS_HREDRAW | CS_VREDRAW;
+    windowWND.lpfnWndProc = windowProc;
+    windowWND.cbClsExtra = 0;
+    windowWND.cbWndExtra = 0;
+    windowWND.hInstance = static_cast<HINSTANCE>( GetModuleHandle( 0 ) );
+    windowWND.hIcon = 0;
+    windowWND.hCursor = ::LoadCursor( 0, IDC_ARROW );
+    windowWND.hbrBackground = reinterpret_cast<HBRUSH>( COLOR_GRAYTEXT + 1 );
+    windowWND.lpszMenuName = 0;
+    windowWND.lpszClassName = L"CMainWindow";
+    windowWND.hIconSm = 0;
+
+    return ::RegisterClassEx( &windowWND ) != 0;
+
+}
+
+bool CMainWindow::Create()
+{
+    handle = ::CreateWindowEx( 0, L"CMainWindow", L"Checkers", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, static_cast<HINSTANCE>( ::GetModuleHandle( 0 ) ), this );
+
+    return handle != 0;
+}
+
+void CMainWindow::Show( int cmdShow ) const
+{
+    ::ShowWindow( handle, cmdShow );
+}
+
+void CMainWindow::OnDestroy() const
+{
+    ::PostQuitMessage( 0 );
+}
+
+LRESULT __stdcall CMainWindow::windowProc( HWND handle, UINT message, WPARAM wParam, LPARAM lParam )
+{
+    switch( message ) {
+		case WM_NCCREATE:
+			::SetWindowLong( handle, GWL_USERDATA, reinterpret_cast<LONG>( reinterpret_cast<CREATESTRUCT*>( lParam )->lpCreateParams ) );
+			return ::DefWindowProc( handle, message, wParam, lParam );
+			break;
+		case WM_DESTROY:
+			reinterpret_cast<CMainWindow*>( ::GetWindowLong( handle, GWL_USERDATA ) )->OnDestroy();
+			break;
+		default:
+			return ::DefWindowProc( handle, message, wParam, lParam );
+			break;
+    }
+    return 0;
+}
---
+++ Checkers/Checkers/MainWindow.h
@@ -0,0 +1,27 @@
+﻿// Автор: Фролов Николай.
+
+// Описание: класс главного окна игры.
+
+#pragma once
+
+#include <Windows.h>
+
+class CMainWindow {
+public:
+	// регистрация класса окна
+    static bool RegisterClass();
+
+    // создание экземпляра окна
+    bool Create();
+
+    // показать окно
+    void Show(int cmdShow) const;
+
+protected:
+    void OnDestroy() const;
+
+private:
+    HWND handle;
+
+    static LRESULT __stdcall windowProc( HWND hanlde, UINT message, WPARAM wParam, LPARAM lParam );
+};
